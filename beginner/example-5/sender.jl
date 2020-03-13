8355c2c9d2db5d1db09535fc70b1b990b0b2f807
Dheepak Krishnamurthy <me@kdheepak.com>
2020-03-13 08:45:29 -0600

    Update files

---
+++ beginner/example-5/sender.jl
@@ -0,0 +1,43 @@
+using HELICS; const h = HELICS
+
+function main()
+
+    initstring = "-f 2 --loglevel=7"
+    broker = h.helicsCreateBroker("zmq", "", initstring)
+    @assert h.helicsBrokerIsConnected(broker) == true
+
+    fedinfo = h.helicsCreateFederateInfo()
+    h.helicsFederateInfoSetCoreType(fedinfo, h.HELICS_CORE_TYPE_ZMQ)
+    h.helicsFederateInfoSetCoreInitString(fedinfo, "--loglevel=7")
+    h.helicsFederateInfoSetTimeProperty(fedinfo, h.HELICS_PROPERTY_TIME_DELTA, 1.0)
+    fed = h.helicsCreateCombinationFederate("sender", fedinfo,)
+
+    topicA = h.helicsFederateRegisterGlobalPublication(
+        fed, "topicA", h.HELICS_DATA_TYPE_DOUBLE, ""
+    )
+
+    h.helicsFederateEnterExecutingMode(fed)
+
+    currenttime = 0
+    for t in 5:10
+        while currenttime < t
+            currenttime = h.helicsFederateRequestTime(fed, t)
+        end
+        println("Sending t = $t at time = $currenttime")
+        h.helicsPublicationPublishDouble(topicA, Float64(t))
+    end
+
+    h.helicsFederateFinalize(fed)
+    h.helicsFederateFree(fed)
+
+    while h.helicsBrokerIsConnected(broker) == true
+        sleep(1)
+    end
+
+    h.helicsBrokerDisconnect(broker)
+
+    h.helicsCloseLibrary()
+
+end
+
+main()
